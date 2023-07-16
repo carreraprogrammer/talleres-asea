3854c1dacefc98d4c2138bcc5e5a59d041466178
Daniel <carreraprogrammer@gmail.com>
2023-07-16 14:14:12 -0500

    Posisionate elements in the menu and add social media icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,77 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import './Navbar.css'
-import { PiSquaresFourFill} from 'react-icons/pi'
-import { CgCloseR } from 'react-icons/cg'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { PiSquaresFourFill } from 'react-icons/pi';
+import { CgCloseR } from 'react-icons/cg';
+import { LuInstagram } from 'react-icons/lu';
+import { LuFacebook } from 'react-icons/lu';
+import { LuLinkedin } from 'react-icons/lu';
+import { LuYoutube } from 'react-icons/lu';
 
 const Navbar = () => {
-  const [showMenu, setShowMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(false);
 
   const handleMenu = () => {
-    setShowMenu(!showMenu)
-    console.log(showMenu)
-  }
+    setShowMenu(!showMenu);
+    console.log(showMenu);
+  };
 
   return (
     <div className='navContainer'>
-      <li className='logo'><Link to="/">Logo</Link></li> 
-      <ul className={showMenu ? 'navBar show' : 'navBar not-show'}>
-         <li className='navLink' onClick={handleMenu}><Link to="/about">Nosotros</Link></li>
-         <li className='navLink' onClick={handleMenu}><Link to="/contact">Contacto</Link></li>
-         <li className='navLink pc'><Link to="/">Logo</Link></li>
-         <li className='navLink mobile'onClick={handleMenu} ><Link to="/">Home</Link></li>
-         <li className='navLink' onClick={handleMenu}><Link to="/products">Productos</Link></li>
-         <li className='navLink' onClick={handleMenu}><Link to="/services">Servicios</Link></li>
-      </ul>
-      <li className={ showMenu ? 'closeBtn show' : 'closeBtn not-show'} onClick={handleMenu}><CgCloseR /></li>
-      <li className='burgerMenu' onClick={handleMenu}>MENU <PiSquaresFourFill /></li>
+      <Link to="/" onClick={() => setShowMenu(false)}>
+        Logo
+      </Link>
+      <div className={showMenu ? 'navBar show' : 'navBar not-show'}>
+        <ul className="navBarLinks">
+          <li className='navLink' onClick={handleMenu}>
+            <Link style={{ color: '#fff' }} to="/about">
+              Nosotros
+            </Link>
+          </li>
+          <li className='navLink' onClick={handleMenu}>
+            <Link style={{ color: '#fff' }} to="/contact">
+              Contacto
+            </Link>
+          </li>
+          <li className='navLink pc'>
+            <Link style={{ color: '#fff' }} to="/">
+              Logo
+            </Link>
+          </li>
+          <li className='navLink mobile' onClick={handleMenu}>
+            <Link style={{ color: '#fff' }} to="/">
+              Home
+            </Link>
+          </li>
+          <li className='navLink' onClick={handleMenu}>
+            <Link style={{ color: '#fff' }} to="/products">
+              Productos
+            </Link>
+          </li>
+          <li className='navLink' onClick={handleMenu}>
+            <Link style={{ color: '#fff' }} to="/services">
+              Servicios
+            </Link>
+          </li>
+        </ul>
+        <div className="socialMedia">
+          <hr className='divider' />
+          <div className="socialMediaIcons">
+            <a><LuYoutube style={{ color: '#fff' }} /></a>
+            <a><LuLinkedin style={{ color: '#fff' }} /></a>
+            <a><LuFacebook style={{ color: '#fff' }} /></a>
+            <a><LuInstagram style={{ color: '#fff' }} /></a>
+          </div>
+        </div>
+      </div>
+      <li className={showMenu ? 'closeBtn show' : 'closeBtn not-show'} style={{ color: '#fff' }} onClick={handleMenu}>
+        <CgCloseR />
+      </li>
+      <li className='burgerMenu' onClick={handleMenu}>
+        MENU <PiSquaresFourFill />
+      </li>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
