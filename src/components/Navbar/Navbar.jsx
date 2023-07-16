821b1668b6eec0f9e0411d03bde438e91f8262e7
Daniel <carreraprogrammer@gmail.com>
2023-07-16 11:36:18 -0500

    Add Products and Services pages

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,9 +5,11 @@ const Navbar = () => {
   return (
     <div>
        <ul>
-          <li className='navLink'><Link to="/">Home</Link></li>
           <li className='navLink'><Link to="/about">Nosotros</Link></li>
           <li className='navLink'><Link to="/contact">Contacto</Link></li>
+          <li className='navLink'><Link to="/">Logo</Link></li>
+          <li className='navLink'><Link to="/projects">Productos</Link></li>
+          <li className='navLink'><Link to="/products">Servicios</Link></li>
        </ul>
     </div>
   )
