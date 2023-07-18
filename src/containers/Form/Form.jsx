211190b615985f11198b40a62d639ad97a26b27d
Daniel <carreraprogrammer@gmail.com>
2023-07-18 11:25:47 -0500

    Finish responsive form

--- src/containers/Form/Form.jsx
+++ src/containers/Form/Form.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Form.css';
 
-const Form = () => {
+const Form = ({ subTitle, title, fields, submitButtonText, onSubmit }) => {
   return (
-    <div>Form</div>
-  )
-}
+    <form className="formContainer" onSubmit={onSubmit}>
+      {subTitle && <p className="formSubtitle">{subTitle}</p>}
+      {title && <h2 className="formTitle">{title}</h2>}
+      {fields.map((field) => (
+        <div className="inputContainer" key={field.name}>
+          {field.type === 'textarea' ? (
+            <textarea className='formTextArea'
+              id={field.name}
+              name={field.name}
+              rows="4"
+              placeholder={field.label}
+            />
+          ) : (
+            <input className='formInput'
+              type={field.type}
+              id={field.name}
+              name={field.name}
+              placeholder={field.label}
+            />
+          )}
+        </div>
+      ))}
+      <button type="submit" className="formSubmitButton">
+        {submitButtonText}
+      </button>
+    </form>
+  );
+};
 
-export default Form
+Form.propTypes = {
+  title: PropTypes.string,
+  fields: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      type: PropTypes.oneOf(['text', 'tel', 'email', 'textarea']).isRequired,
+    })
+  ).isRequired,
+  submitButtonText: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+Form.defaultProps = {
+  title: '',
+};
+
+export default Form;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,15 +3,35 @@ import Header from '../../containers/Header/Header'
 import Projects from '../../containers/ProjectsGallery/ProjectsGallery'
 import AboutAbstract from '../../components/AboutAbstract/AboutAbstract'
 import ServicesAbstract from '../../components/ServicesAbstract/ServicesAbstract'
+import Form from '../../containers/Form/Form'
 import './HomePage.css'
 
 const HomePage = () => {
+  const formFields = [
+    { name: 'name', label: 'Nombre', type: 'text' },
+    { name: 'phone', label: 'Teléfono', type: 'tel' },
+    { name: 'email', label: 'Email', type: 'email' },
+    { name: 'message', label: 'Mensaje', type: 'textarea' },
+  ];
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes manejar el envío del formulario, por ejemplo, enviando los datos al servidor.
+  };
+
   return (
   <div className='homeContainer'>
     <Header />
     <AboutAbstract />
     <Projects />
     <ServicesAbstract />
+    <Form
+      subTitle="CONTACTO"
+      title="Sientase Libre De contactarnos"
+      fields={formFields}
+      submitButtonText="ENVIA TU MENSAJE"
+      onSubmit={handleSubmit}
+    />
   </div>  
   )
 }
