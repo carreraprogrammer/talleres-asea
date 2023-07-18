f2fb5970713fae2f9550d1edbd4179394c859f7a
Daniel <carreraprogrammer@gmail.com>
2023-07-17 20:00:05 -0500

    Use filters instead of images

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import headerBackground from '../../assets/header_background.png';
 
 const Header = () => {
   return (
-    <div className='headerContainer'>
-      <h1 className='headerTitle'>Más De 70 Años En La Industria.</h1>
-      <p className='headerSubtitle'>Somos una empresa familiar con más de 70 años de experiencia en la industria de la metalmecánica y la construcción. Durante décadas, hemos brindado soluciones innovadoras y de alta calidad a nuestros clientes, estableciendo una reputación sólida y confiable en el mercado. </p>
+    <div className="headerContainer">
+      <h1 className="headerTitle">Más De 70 Años En La Industria.</h1>
+      <p className="headerSubtitle">
+        Somos una empresa familiar con más de 70 años de experiencia en la industria de la metalmecánica y la construcción. Durante décadas, hemos
+        brindado soluciones innovadoras y de alta calidad a nuestros clientes, estableciendo una reputación sólida y confiable en el mercado.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/containers/ProjectsGallery/ProjectsGallery.jsx
+++ src/containers/ProjectsGallery/ProjectsGallery.jsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './ProjectsGallery.css';
 import rumichacaIpiales from '../../assets/carretera_rumichaca_ipiales.png';
-import rumichacaIpialesHover from '../../assets/rumichaca_ipiales_gallery.png';
 import udenar from '../../assets/udenar.png';
-import udenarHover from '../../assets/udenar_gallery.png';
 import { BsArrowLeftShort } from 'react-icons/bs';
 import { BsArrowRightShort } from 'react-icons/bs';
 
@@ -13,44 +11,27 @@ const ProjectsGallery = () => {
       title: 'Consorcio SH',
       description: 'Construimos formaletas de alta calidad para cajas de iluminación y fibra óptica, viaductos para la segunda calzada Pasto-Ipiales, señalización vial y estructuras metálicas para cubiertas de bodegas.',
       image: rumichacaIpiales,
-      imageHover: rumichacaIpialesHover
     },
     udenar: {
       title: 'Universidad de Nariño',
       description: 'Construimos e instalamos aproximadamente 60.000 kilos de estructura metálica para la Universidad de Nariño en el municipio de Tumaco, Nariño.',
       image: udenar,
-      imageHover: udenarHover
-    }
+    },
   };
   const projectsKeys = Object.keys(projectsObj);
-  const [isHovered, setIsHovered] = useState(false);
   const [projectKey, setProjectKey] = useState(projectsKeys[0]);
 
   useEffect(() => {
-    let interval;
-
-    const handleMouseLeave = () => {
+    const interval = setInterval(() => {
       const currentIndex = projectsKeys.indexOf(projectKey);
       const nextIndex = (currentIndex + 1) % projectsKeys.length;
       setProjectKey(projectsKeys[nextIndex]);
-    };
-
-    if (!isHovered) {
-      interval = setInterval(handleMouseLeave, 4000);
-    }
+    }, 4000); // Cambiar el valor de 4000 para ajustar el intervalo (en milisegundos)
 
     return () => {
       clearInterval(interval);
     };
-  }, [isHovered, projectKey, projectsKeys]);
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
+  }, [projectKey, projectsKeys]);
 
   const handleArrowLeftClick = () => {
     const currentIndex = projectsKeys.indexOf(projectKey);
@@ -67,18 +48,13 @@ const ProjectsGallery = () => {
   const project = projectsObj[projectKey];
 
   return (
-    <div
-      className="galleryContainer"
-      style={{ backgroundImage: `url(${isHovered ? project.imageHover : project.image})` }}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
+    <div className="galleryContainer" style={{ backgroundImage: `url(${project.image})` }}>
       <div className="gallery"></div>
       <div className="arrowLeftContainer" onClick={handleArrowLeftClick}>
         <BsArrowLeftShort className="arrowLeft" />
       </div>
       <div className="projectInfo">
-        <p className='projectSubtitle'>QUE PODRIAMOS HACER POR TI?</p>
+        <p className="projectSubtitle">QUE PODRIAMOS HACER POR TI?</p>
         <h3 className="projectTitle">{project.title}</h3>
         <p className="projectDescription">{project.description}</p>
       </div>
