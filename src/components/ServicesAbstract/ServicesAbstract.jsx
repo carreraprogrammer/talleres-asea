473ec7a3531ed546b032f936b2e1a34efb21e7a2
Daniel <carreraprogrammer@gmail.com>
2023-07-17 21:05:49 -0500

    Correct bugs

--- src/components/ServicesAbstract/ServicesAbstract.jsx
+++ src/components/ServicesAbstract/ServicesAbstract.jsx
@@ -7,13 +7,13 @@ const ServicesAbstract = () => {
       <div className="servicesAbstractTitleContainer">
         <h2 className="servicesAbstractTitle">NUESTROS SERVICIOS</h2>
         <h3 className="servicesAbstractSubtitle">
-          Ofrecemos
+          OFRECEMOS
         </h3>
       </div>
       <div className="servicesAbstractParagraphContainer">
-        <p className="servicesAbstractParagraph">En nuestra empresa, nos dedicamos a la fabricación de maquinaria para la construcción y maquinaria agrícola, así como a la producción de compuertas y implementos sanitarios para acueductos. Nuestros productos de alta calidad incluyen mezcladoras, plumas grúas, cortadoras de concreto y reglas vibratorias, entre otros equipos esenciales para diversos proyectos.</p> 
-        <p className="servicesAbstractParagraph">Además, ofrecemos servicios especializados en la construcción y reconstrucción de partes y piezas para maquinaria agrícola, industrial y de la construcción, lo que garantiza la prolongación de la vida útil de sus equipos. Contamos con un equipo altamente capacitado que realiza mecánica industrial, torno, fresa, soldadura y fundición de hierro gris, bronce y aluminio.</p> 
-        <p className="servicesAbstractParagraph">Gracias a nuestra experiencia de más de 75 años en el sector, somos reconocidos como líderes en la industria metalúrgica y mecánica en la región del suroccidente colombiano. Nuestro compromiso es brindar soluciones integrales y personalizadas a nuestros clientes, respaldados por tecnología de vanguardia y un enfoque en la calidad y la eficiencia en cada uno de nuestros servicios.</p>
+        <p className="servicesAbstractParagraph">Fabricamos maquinaria para la construcción y agrícola, compuertas e implementos sanitarios. Realizamos montajes industriales de puentes y estructuras metálicas.</p> 
+        <p className="servicesAbstractParagraph">Ofrecemos servicios de construcción y reconstrucción de piezas para maquinaria agrícola, industrial y de la construcción, con especialización en mecánica industrial, torno, fresa, soldadura y fundición.</p> 
+        <p className="servicesAbstractParagraph">Somos líderes en la industria metalúrgica y mecánica del suroccidente colombiano. Brindamos soluciones integrales y personalizadas con tecnología de vanguardia.</p>
       </div>
     </div>
   )
