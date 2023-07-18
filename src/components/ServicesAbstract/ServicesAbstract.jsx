4f84efa01b6746e32cf66a16abf00437d54b07be
Daniel <carreraprogrammer@gmail.com>
2023-07-18 12:12:29 -0500

    Update checklist services"

--- src/components/ServicesAbstract/ServicesAbstract.jsx
+++ src/components/ServicesAbstract/ServicesAbstract.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
-import './ServicesAbstract.css'
+import React from 'react';
+import { BsCheck } from 'react-icons/bs'; // Importa el ícono de react-icons
+import './ServicesAbstract.css';
 
 const ServicesAbstract = () => {
   return (
     <div className="servicesAbstractContainer">
       <div className="servicesAbstractTitleContainer">
         <h2 className="servicesAbstractTitle">NUESTROS SERVICIOS</h2>
-        <h3 className="servicesAbstractSubtitle">
-          OFRECEMOS
-        </h3>
+        <h3 className="servicesAbstractSubtitle">NOS ESPECIALIZAMOS EN</h3>
       </div>
-      <div className="servicesAbstractParagraphContainer">
-        <p className="servicesAbstractParagraph">Fabricamos maquinaria para la construcción y agrícola, compuertas e implementos sanitarios. Realizamos montajes industriales de puentes y estructuras metálicas.</p> 
-        <p className="servicesAbstractParagraph">Ofrecemos servicios de construcción y reconstrucción de piezas para maquinaria agrícola, industrial y de la construcción, con especialización en mecánica industrial, torno, fresa, soldadura y fundición.</p> 
-        <p className="servicesAbstractParagraph">Somos líderes en la industria metalúrgica y mecánica del suroccidente colombiano. Brindamos soluciones integrales y personalizadas con tecnología de vanguardia.</p>
+      <div className="servicesAbstractListContainer">
+        <ul className="servicesAbstractList">
+          <li className="servicesAbstractListItem">
+            <BsCheck className="checkIcon" />
+            <p>Fabricación de maquinaria para la construcción (mezcladoras, plumas grúas, cortadoras de concreto, reglas vibratorias, entre otros).</p>
+          </li>
+          <li className="servicesAbstractListItem">
+            <BsCheck className="checkIcon" />
+            <p>Fabricación de maquinaria agrícola</p>
+          </li>
+          <li className="servicesAbstractListItem">
+            <BsCheck className="checkIcon" />
+            <p>Fabricación de compuertas</p>
+          </li>
+          <li className="servicesAbstractListItem">
+            <BsCheck className="checkIcon" />
+            <p>Fabricación de implementos sanitarios y para acueductos</p>
+          </li>
+          <li className="servicesAbstractListItem">
+            <BsCheck className="checkIcon" />
+            <p>Fabricación e instalación de estructuras metálicas.</p>
+          </li>
+          <li className="servicesAbstractListItem">
+            <BsCheck className="checkIcon" />
+            <p>Construcción y reconstrucción de partes y piezas para maquinaria agrícola, industrial y de la construcción.</p>
+          </li>
+          <li className="servicesAbstractListItem">
+            <BsCheck className="checkIcon" />
+            <p>Fabricación, instalación y montajes industriales de puentes carreteros y peatonales</p>
+          </li>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ServicesAbstract
+export default ServicesAbstract;
