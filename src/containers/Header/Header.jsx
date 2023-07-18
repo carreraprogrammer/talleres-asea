01560129f9f38c3b457aedf56cb85c9b51131154
Daniel <carreraprogrammer@gmail.com>
2023-07-17 20:10:24 -0500

    Improve header

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -5,7 +5,10 @@ import headerBackground from '../../assets/header_background.png';
 const Header = () => {
   return (
     <div className="headerContainer">
-      <h1 className="headerTitle">Más De 70 Años En La Industria.</h1>
+      <div>
+        <p className='mainTitle'>SOMOS TALLERES ASEA</p>
+        <h1 className="headerTitle">Más De 70 Años En La Industria.</h1>
+      </div>   
       <p className="headerSubtitle">
         Somos una empresa familiar con más de 70 años de experiencia en la industria de la metalmecánica y la construcción. Durante décadas, hemos
         brindado soluciones innovadoras y de alta calidad a nuestros clientes, estableciendo una reputación sólida y confiable en el mercado.
